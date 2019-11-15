453cb5e757043718f1ec127226407831f7d10ecc
Tanya Borrego <tre_585@hotmail.com>
2019-11-14 19:15:05 -0500

    111419_Release

--- maintest.js
+++ maintest.js
@@ -3,25 +3,20 @@ const tasks = [];
 const addTask = (ev)=>{
     ev.preventDefault();
     const newTask = {
-        id: Date.now(),
         title: document.getElementById('newTask').value,
         date:  document.getElementById('date').value,
     
     }
 
-    tasks.push(newTask);
-    document.forms[0].reset();
-
-
+   tasks.push(newTask);
+ 
 
 localStorage.setItem('MyNewTasks', JSON.stringify(tasks) );
-
-}
-
-const addRow = (ev)=> {
-    const id = Date.now();
+};
+const addRow = (ev)=>{
+    ev.preventDefault();
     const newTask = document.getElementById('newTask').value;
-    const date = document.getElementById('date').value; 
+    const date = document.getElementById('date').value;
 
     const table = document.getElementsByTagName('table')[0];
     
@@ -29,18 +24,17 @@ const addRow = (ev)=> {
         
     const cel1 = newRow.insertCell(0)
     const cel2 = newRow.insertCell(1);
-    const cel3 = newRow.insertCell(2);
-        
-        cel1.innerHTML = id;
-        cel2.innerHTML = newTask;
-        cel3.innerHTML = date;
-}
+            
+        cel1.innerHTML = newTask;
+        cel2.innerHTML = date;
+     
+};
+
 document.addEventListener('DOMContentLoaded', ()=>{
-    const subButton = document.getElementById ("subButton");
-    subButton.addEventListener('click', addTask);
-    subButton.addEventListener('click', addRow);
-}
-);
+    document.getElementById('subButton').addEventListener('click', addTask);
+    document.getElementById('subButton').addEventListener('click', addRow);
+    
+})
   //next steps - get task and date value, store in an array
     //write display function to build table i can see
     //validate data
